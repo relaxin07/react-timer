ff5008b0ebe6ada4c51289561412f727688cb30a
relax1n <relaxinfh@mail.ru>
2020-02-27 02:46:11 +0200

    add action creator task

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,37 +3,44 @@ const worker = () => {
     return {
     type : 'THIS_WORK',
     }
-}
+};
 
 const deleteItem = ( payload ) =>{
-    console.log(payload)
     return{
         type: 'DELETE_ITEM',
         payload
     }
-}
+};
 
-const addItem = (taskName ,timeStart , timeEnd , timeSpend) =>{
+const addItem = (taskId,taskName ,timeStart , timeEnd , timeSpend) =>{
     return {
         type:"ADD_ITEM",
+        taskId,
         taskName,
         timeStart,
         timeEnd,
         timeSpend,
 
     }
-}
+};
 
 const showModal = () =>{
     return{
         type: "SHOW_MODAL",
     }
-}
+};
 
 const getTask = ( payload) => {
     return {
         type: "GET_TASK",
         payload,
+    }
+};
+
+const generateTasks = (payload) =>{
+    return{
+        type:'GENERATE_TASKS',
+        payload
     }
 }
 
@@ -42,5 +49,6 @@ export {
     deleteItem,
     addItem,
     showModal,
-    getTask
+    getTask,
+    generateTasks,
 };
