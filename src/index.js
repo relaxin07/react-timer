cd837dd391e6c671570c88700b47e3742171ee80
relax1n <relaxinfh@mail.ru>
2020-03-30 18:09:32 +0300

    change path store

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
-import {Provider} from 'react-redux';
-import {BrowserRouter as Router } from 'react-router-dom';
-import store from './store';
-
-
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from './store/store';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router basename={'timer-task'}>
-            <App/>
-        </Router>
-    </Provider>,
-document.getElementById('root'))
+  <Provider store={store}>
+    <Router basename={'timer-task'}>
+      <App/>
+    </Router>
+  </Provider>,
+  document.getElementById('root'));
