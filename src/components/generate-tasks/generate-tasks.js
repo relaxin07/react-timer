41881c18fcb973fa410f6944350c66bc50c90488
relax1n <relaxinfh@mail.ru>
2020-03-30 18:05:20 +0300

    chane func  createTask and cnvrTime

--- src/components/generate-tasks/generate-tasks.js
+++ src/components/generate-tasks/generate-tasks.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { actions } from '../../reducers/reducer';
+import { actions } from '../../store/modules/tasks/index';
 import { bindActionCreators } from 'redux';
 import './generate-task.css';
 
@@ -10,42 +10,50 @@ function createRandomNmbr(min, max) {
   return Math.round(rand);
 }
 
-function convertorTime(minutes) {
-  let h = minutes / 60 ^ 0;
-  let m = minutes > 59 ? minutes - 60 : minutes;
+function convertorTime(time) {
+  let seconds = Math.round(time / 1000);
+  let h = seconds / 3600 ^ 0;
+  let m = (seconds - h * 3600) / 60 ^ 0;
+  let s = (seconds - h * 3600) % 60;
   if (h < 10) h = '0' + h;
   if (m < 10) m = '0' + m;
-  return `${h}:${m}:00`;
+  if (s < 10) s = '0' + s;
+  return `${h}:${m}:${s}`;
 }
 
 
 function createTask(action) {
   let countNumber = createRandomNmbr(10, 15);
   let tasks = [];
+  let timeStart = new Date();
+  timeStart.setHours(createRandomNmbr(6, 9), [0], [0]);
+
   for (let i = 1; i <= countNumber; i++) {
+    let timeEnd = new Date();
+    timeEnd.setHours(timeStart.getHours(), [timeStart.getMinutes() + createRandomNmbr(10, 90)], [timeStart.getSeconds()]);
     tasks.push({
       id: `${i}task${i}`,
+      timeStart: timeStart.toLocaleTimeString(),
       taskName: `Task ${i}`,
-      timeSpend: convertorTime(createRandomNmbr(30, 90)),
+      timeEnd: timeEnd.toLocaleTimeString(),
+      timeSpend: convertorTime(timeEnd - timeStart),
     });
+    timeStart.setHours(timeEnd.getHours(), [timeEnd.getMinutes() + createRandomNmbr(5, 10)], [timeEnd.getSeconds()]);
   }
   action(tasks);
   return tasks;
 }
 
-
-const GenerateTasks = ({ generateTaskThunk }) => {
+const GenerateTasks = ({ generateTasks }) => {
   return (
-    <button className="generate-btn" onClick={() => createTask(generateTaskThunk)}>Generate</button>
+    <button className="generate-btn" onClick={() => createTask(generateTasks)}>Generate</button>
   );
 };
 
-
 const mapDispatchToProps = (dispatch) => {
-  const { generateTasks, generateTaskThunk } = bindActionCreators(actions, dispatch);
+  const { generateTasks } = bindActionCreators(actions, dispatch);
   return {
     generateTasks,
-    generateTaskThunk,
   };
 };
 
