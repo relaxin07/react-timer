3956152842ed2bb2467c1bb8c33dc4bcd25affef
relax1n <relaxinfh@mail.ru>
2020-03-30 18:02:06 +0300

    add style to btn(back home)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,32 +1,29 @@
-import React from "react";
-import {Route , Switch,Link} from 'react-router-dom';
-import {homePage, chartPage, detailTaskPage} from '../../pages'
+import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+import { homePage, chartPage, detailTaskPage } from '../../pages';
 import MyTabs from '../tabs';
 import Timer from '../timer';
 
-
-
 import './app.css';
 
-
 const App = () => {
-    return (
-        <div className="container">
-            <Timer/>
-            <MyTabs/>
-            <Switch>
-                <Route path="/" component={homePage} exact/>
-                <Route path="/chart-page" component={chartPage}/>
-                <Route path="/tasks/:id" component={detailTaskPage}/>
-                <Route path="*"> no match <br />
-                <Link to="/">
-                    Back home
-                </Link>
-                </Route>
-            </Switch>
-
-        </div>
-    )
+  return (
+    <div className="container">
+      <Timer/>
+      <MyTabs/>
+      <Switch>
+        <Route path="/" component={homePage} exact/>
+        <Route path="/chart-page" component={chartPage}/>
+        <Route path="/tasks/:id" component={detailTaskPage}/>
+        <Route path="*"> <br/> Page is undefined <br/>
+          <Link to="/" className="back-home-link">
+            Back home
+          </Link>
+        </Route>
+      </Switch>
+    </div>
+  );
 };
+
 
 export default App;
